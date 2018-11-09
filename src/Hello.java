f57624b4bcb67d5e2f615cfa7bcec1c4ac479f6b
CezarGeorgescu <georgescucm@gmail.com>
2018-11-09 19:37:48 +0200

    --

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,25 @@
 public class Hello {
     public static void main(String[] args) {
-        int myAge = 22;
+        int myAge;
+
         float weight = 60.5f;
         double height = 356.6;
         char gender= 'm';
         char test = 9;
         boolean isStudent = true;
         boolean hasJob = false;
-        int yourAge = 33;
+        String mesaj = "hello";
+        String mesaj2 ="goodbye";
+        String mesaj3 = mesaj + mesaj2;
+        System.out.println(mesaj3);
+        double a = 5;
+        int b = 2;
+        double c = a / b;
+        System.out.println(c);
+        int x = 10;
+        int y = --x;
+
+        System.out.println(y);
+
     }
 }
