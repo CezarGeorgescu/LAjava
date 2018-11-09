66f685274c0a91861e750ebe9def947acd746a31
CezarGeorgescu <georgescucm@gmail.com>
2018-11-09 17:56:35 +0200

    -

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+       int myAge = 22;
+       float weight = 60.5f;
+       double height = 356.6;
+       long distance = 12131241390412913l;
+       char gender= 'm';
+       char test = 9;
+       boolean isStudent = true;
+       boolean hasJob = false;
+    }
+}
