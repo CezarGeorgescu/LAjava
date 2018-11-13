af5f91bdb3e2d59b092a32bac898359ccb4400b1
CezarGeorgescu <georgescucm@gmail.com>
2018-11-13 19:39:18 +0200

    --

--- src/Switch.java
+++ src/Switch.java
@@ -32,12 +32,9 @@ public class Switch {
   int i,j;
   for( i=1;i<=9;i++)
   { for( j=1;j<=9;j++){
-      System.out.print(i);
-      System.out.print(" x ");
-      System.out.print(j);
-      System.out.print(" = ");
-      System.out.println(i*j);
-        }}
+      System.out.println(i + " x " + j + " = " + i*j);
+        }
+     }
 
     }
 
